07ab2f63cfe568f46a3a1f44b7fdb72e6bf5ad13
Garfield <gblake77@gmail.com>
2024-08-18 10:29:00 +0100

    Initial commit

--- cypress/support/pageObjects/resultsPage.js
+++ cypress/support/pageObjects/resultsPage.js
@@ -5,7 +5,7 @@ export class resultsPage {
     cy.get('[class="sc-iLIByi kBtGYR"]').should('contain', 'Displaying results for');
     cy.get('[class="sc-iLIByi kBtGYR"]').should('contain','investers');
     cy.get('[data-testid="tabsHandlers-tabPanel-nowResults"]').should('be.visible')
-    }
+      }
    
    
    
